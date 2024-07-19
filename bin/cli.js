09841823df09f6fec52934ab28c93de1ae4ed9eb
Eze <ezequiel.n.villa@gmail.com>
2024-07-19 09:35:53 -0300

    Change in cli

--- bin/cli.js
+++ bin/cli.js
@@ -21,25 +21,27 @@ const questions = [
   },
 ];
 
+const projectName = process.argv[2];
+const gitCloneCommand = `git clone --depth 1 https://github.com/EzequielVilla/base-express-backend ${projectName}`;
+const installCommand = `cd ${projectName} && npm install`;
+console.log("Cloning repository...");
+const checkedOut = runCommand(gitCloneCommand);
+if (!checkedOut) process.exit(-1);
+const installedDeps = runCommand(installCommand);
+if (!installedDeps) process.exit(-1);
+console.log("Project created successfully with the following configuration:");
+
 // const installInquirer = `npm install inquirer`;
 // const inquirerInstalled = runCommand(installInquirer);
 // if (!inquirerInstalled) process.exit(-1);
 // const gitCloneCommand = `git clone --depth 1 https://github.com/EzequielVilla/base-express-backend ${projectName}`;
 
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    const { projectName } = answers;
-    const gitCloneCommand = `git clone --depth 1 https://github.com/EzequielVilla/base-express-backend ${projectName}`;
-    const installCommand = `cd ${projectName} && npm install`;
-    console.log("Cloning repository...");
-    const checkedOut = runCommand(gitCloneCommand);
-    if (!checkedOut) process.exit(-1);
-    const installedDeps = runCommand(installCommand);
-    if (!installedDeps) process.exit(-1);
-    console.log("Project created successfully with the following configuration:");
-    console.log(`Project Name: ${projectName}`);
-  })
-  .catch((error) => {
-    console.error("Error creating project:", error);
-  });
+// inquirer
+//   .prompt(questions)
+//   .then((answers) => {
+//     const { projectName } = answers;
+//     console.log(`Project Name: ${projectName}`);
+//   })
+//   .catch((error) => {
+//     console.error("Error creating project:", error);
+//   });
