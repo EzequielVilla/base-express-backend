1bb9aa337c883efc8b6ad373e9ac06729269150d
Eze <ezequiel.n.villa@gmail.com>
2024-07-19 09:19:13 -0300

    Update version and cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -21,6 +21,11 @@ const questions = [
   },
 ];
 
+const installInquirer = `npm install inquirer`;
+const inquirerInstalled = runCommand(installInquirer);
+if (!inquirerInstalled) process.exit(-1);
+// const gitCloneCommand = `git clone --depth 1 https://github.com/EzequielVilla/base-express-backend ${projectName}`;
+
 inquirer
   .prompt(questions)
   .then((answers) => {
