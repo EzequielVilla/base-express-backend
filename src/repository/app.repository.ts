c83e680a0e0116d9c22b77a6ad3380ed96b3a9b1
Eze <ezequiel.n.villa@gmail.com>
2024-07-25 14:25:19 -0300

    Add connection to DB and import in app.repository

--- src/repository/app.repository.ts
+++ src/repository/app.repository.ts
@@ -1,5 +1,12 @@
+import { App } from "../model";
+
 export class AppRepository {
   async healthCheck() {
-    return "Health check ok";
+    try {
+      return "Health check ok";
+    } catch (error) {
+      console.log(error);
+      throw new Error("ERROR_IN_HEALTH_CHECK");
+    }
   }
 }
